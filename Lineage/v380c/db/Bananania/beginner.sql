0b79e237fc6c801587568b4595e62d521a611657
raybinking <raybinking@gmail.com>
2024-03-02 01:36:31 +0800

    update

--- Lineage/v380c/db/Bananania/beginner.sql
+++ Lineage/v380c/db/Bananania/beginner.sql
@@ -32,38 +32,12 @@ CREATE TABLE `beginner` (
 -- ----------------------------
 -- Records 
 -- ----------------------------
-INSERT INTO `beginner` VALUES ('1', '40005', '1', '0', '0', '蠟燭', 'A', '1');
-INSERT INTO `beginner` VALUES ('2', '40005', '1', '0', '0', '蠟燭', 'A', '1');
-INSERT INTO `beginner` VALUES ('3', '40641', '1', '0', '0', '說話卷軸', 'A', '1');
-INSERT INTO `beginner` VALUES ('4', '35', '1', '0', '0', '象牙塔單手劍', 'K', '1');
-INSERT INTO `beginner` VALUES ('5', '48', '1', '0', '0', '象牙塔雙手劍', 'K', '1');
-INSERT INTO `beginner` VALUES ('6', '147', '1', '0', '0', '象牙塔斧頭', 'K', '1');
-INSERT INTO `beginner` VALUES ('7', '105', '1', '0', '0', '象牙塔長矛', 'K', '1');
-INSERT INTO `beginner` VALUES ('8', '174', '1', '0', '0', '象牙塔石弓', 'K', '1');
-INSERT INTO `beginner` VALUES ('9', '7', '1', '0', '0', '象牙塔短劍', 'K', '1');
-INSERT INTO `beginner` VALUES ('10', '49309', '1', '0', '0', '象牙塔箭筒', 'K', '1');
-INSERT INTO `beginner` VALUES ('11', '35', '1', '0', '0', '象牙塔單手劍', 'P', '1');
-INSERT INTO `beginner` VALUES ('12', '48', '1', '0', '0', '象牙塔雙手劍', 'P', '1');
-INSERT INTO `beginner` VALUES ('13', '147', '1', '0', '0', '象牙塔斧頭', 'P', '1');
-INSERT INTO `beginner` VALUES ('14', '7', '1', '0', '0', '象牙塔短劍', 'P', '1');
-INSERT INTO `beginner` VALUES ('15', '35', '1', '0', '0', '象牙塔單手劍', 'E', '1');
-INSERT INTO `beginner` VALUES ('16', '175', '1', '0', '0', '象牙塔長弓', 'E', '1');
-INSERT INTO `beginner` VALUES ('17', '174', '1', '0', '0', '象牙塔石弓', 'E', '1');
-INSERT INTO `beginner` VALUES ('18', '7', '1', '0', '0', '象牙塔短劍', 'E', '1');
-INSERT INTO `beginner` VALUES ('19', '49309', '1', '0', '0', '象牙塔箭筒', 'E', '1');
-INSERT INTO `beginner` VALUES ('20', '35', '1', '0', '0', '象牙塔單手劍', 'W', '1');
-INSERT INTO `beginner` VALUES ('21', '224', '1', '0', '0', '象牙塔魔杖', 'W', '1');
-INSERT INTO `beginner` VALUES ('22', '7', '1', '0', '0', '象牙塔短劍', 'W', '1');
-INSERT INTO `beginner` VALUES ('23', '35', '1', '0', '0', '象牙塔單手劍', 'D', '1');
-INSERT INTO `beginner` VALUES ('24', '174', '1', '0', '0', '象牙塔石弓', 'D', '1');
-INSERT INTO `beginner` VALUES ('25', '73', '1', '0', '0', '象牙塔雙刀', 'D', '1');
-INSERT INTO `beginner` VALUES ('26', '156', '1', '0', '0', '象牙塔鋼爪', 'D', '1');
-INSERT INTO `beginner` VALUES ('27', '7', '1', '0', '0', '象牙塔短劍', 'D', '1');
-INSERT INTO `beginner` VALUES ('28', '49309', '1', '0', '0', '象牙塔箭筒', 'D', '1');
-INSERT INTO `beginner` VALUES ('29', '35', '1', '0', '0', '象牙塔單手劍', 'R', '1');
-INSERT INTO `beginner` VALUES ('30', '48', '1', '0', '0', '象牙塔雙手劍', 'R', '1');
-INSERT INTO `beginner` VALUES ('31', '147', '1', '0', '0', '象牙塔斧頭', 'R', '1');
-INSERT INTO `beginner` VALUES ('32', '147', '1', '0', '0', '象牙塔斧頭', 'I', '1');
-INSERT INTO `beginner` VALUES ('33', '174', '1', '0', '0', '象牙塔石弓', 'I', '1');
-INSERT INTO `beginner` VALUES ('34', '224', '1', '0', '0', '象牙塔魔杖', 'I', '1');
-INSERT INTO `beginner` VALUES ('35', '49309', '1', '0', '0', '象牙塔箭筒', 'I', '1');
+INSERT INTO `beginner` VALUES ('1', '40641', '1', '0', '0', '說話卷軸', 'A', '1');
+INSERT INTO `beginner` VALUES ('2', '49309', '1', '0', '0', '象牙塔箭筒', 'E', '1');
+INSERT INTO `beginner` VALUES ('3', '289', '1', '0', '0', '耀武短劍', 'P', '1');
+INSERT INTO `beginner` VALUES ('4', '290', '1', '0', '0', '耀武雙手劍', 'K', '1');
+INSERT INTO `beginner` VALUES ('5', '292', '1', '0', '0', '耀武之弩', 'E', '1');
+INSERT INTO `beginner` VALUES ('6', '293', '1', '0', '0', '耀武魔杖', 'W', '1');
+INSERT INTO `beginner` VALUES ('7', '291', '1', '0', '0', '耀武雙刀', 'D', '1');
+INSERT INTO `beginner` VALUES ('8', '289', '1', '0', '0', '耀武短劍', 'R', '1');
+INSERT INTO `beginner` VALUES ('9', '292', '1', '0', '0', '耀武之弩', 'I', '1');
