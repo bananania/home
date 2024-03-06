3b718a830d93b52820792d274f852d619445ac33
bananania <banananiaGM@gmail.com>
2024-03-06 10:08:44 +0800

    Update L1Shop.java

--- Lineage/v380c/src/l1j/server/server/model/shop/L1Shop.java
+++ Lineage/v380c/src/l1j/server/server/model/shop/L1Shop.java
@@ -147,8 +147,8 @@ public class L1Shop {
 		if ( (Config.ALT_BANANA == true) && ((_npcId >= Config.ALT_BANANA_MIN_ID) && (_npcId <= Config.ALT_BANANA_MAX_ID)) ) {
 			if (!pc.getInventory().checkItem(L1ItemId.BANANA, price)) {
 			    System.out.println(price);
-			    // \f1アデナが不足しています。
-			    pc.sendPackets(new S_ServerMessage(189));
+			    // \f1アデナが不足しています。			    
+			    pc.sendPackets(new S_SystemMessage((new StringBuilder()).append("香蕉幣不足。").toString())); //pc.sendPackets(new S_ServerMessage(189));
 			    return false;
 		    }
 		}
